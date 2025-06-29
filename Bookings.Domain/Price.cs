78f18c88d0b7f2e9defa20814fc14e26e9d59a8c
Vyacheslav Shengur <slavcho21@gmail.com>
2025-06-29 17:01:37 +0300

    Updated price doamin object

--- Bookings.Domain/Price.cs
+++ Bookings.Domain/Price.cs
@@ -1,14 +1,22 @@
 ﻿namespace Bookings.Domain;
 
-public class Price : BaseObject
+/// <summary>
+/// Доменная сущность цены
+/// </summary>
+public record Price : BaseObject
 {
+    public Room Room { get; init; } = null!;
+    public double Amount { get; init; }
+    public DateOnly StartDate { get; init; }
+    public DateOnly EndDate { get; init; }
+
     public Price() : base() { }
 
-    public Room Room { get; set; } = null!;
-
-    public double Amount { get; set; }
-
-    public DateOnly StartDate { get; set; }
-
-    public DateOnly EndDate { get; set; }
+    public Price(Room room, double amount, DateOnly startDate, DateOnly endDate) : base()
+    {
+        Room = room;
+        Amount = amount;
+        StartDate = startDate;
+        EndDate = endDate;
+    }
 }
